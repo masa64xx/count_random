ef38437be603c393e406a3db13105e132eedfe89
masa64xx <murakamimasanori@murakami-no-MacBook-Pro.local>
2015-07-08 23:58:59 +0900

    はい

--- count_tsuika/ViewController.swift
+++ count_tsuika/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var number: Int32 = 0
+    var audioplayer : AVAudioPlayer!
+    @IBOutlet var backgroundColor: UIColor!
+    @IBOutlet var setTitleColor: UIColor!
 
+    @IBOutlet var label: UILabel!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        let soundFilePath : NSString =
+        NSBundle.mainBundle().pathForResource("voise", ofType: "mp3")!
+        let fileURL : NSURL = NSURL(fileURLWithPath: soundFilePath as String)!
+        // AVAudioPlayerのインスタンス化.
+        audioplayer = AVAudioPlayer(contentsOfURL: fileURL, error: nil)
+
+            }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func random() {
+        var randInt = Int32(arc4random_uniform(4))+1
+        number = number + randInt
+        label.text = String (number)
+        if (number%7==0){
+            view.backgroundColor = UIColor.blackColor()
+            label.textColor = UIColor.whiteColor()
+            audioplayer.play()
+            }else{
+            view.backgroundColor = UIColor.whiteColor()
+            label.textColor = UIColor.blackColor()
+        }
+                      }
+    
+        }
 
-
-}
+    
 
