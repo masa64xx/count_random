4bbc8d99d543403b71fa843b0e6470ebb7a7e977
masa64xx <murakamimasanori@murakami-no-MacBook-Pro.local>
2015-07-09 16:55:26 +0900

    はい

--- count_tsuika/ViewController.swift
+++ count_tsuika/ViewController.swift
@@ -13,9 +13,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     var number: Int32 = 0
     var audioplayer : AVAudioPlayer!
-    @IBOutlet var backgroundColor: UIColor!
-    @IBOutlet var setTitleColor: UIColor!
-
+   
     @IBOutlet var label: UILabel!
   
     override func viewDidLoad() {
@@ -27,28 +25,28 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         // AVAudioPlayerのインスタンス化.
         audioplayer = AVAudioPlayer(contentsOfURL: fileURL, error: nil)
 
-            }
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+        }
     
     @IBAction func random() {
         var randInt = Int32(arc4random_uniform(4))+1
-        number = number + randInt
-        label.text = String (number)
-        if (number%7==0){
+            number = number + randInt
+            label.text = String (number)
+            if (number%7==0){
             view.backgroundColor = UIColor.blackColor()
             label.textColor = UIColor.whiteColor()
             audioplayer.play()
             }else{
             view.backgroundColor = UIColor.whiteColor()
             label.textColor = UIColor.blackColor()
+            }
         }
-                      }
     
-        }
+    }
 
     
 
